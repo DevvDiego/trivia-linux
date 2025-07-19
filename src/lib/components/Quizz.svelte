f9c69ebd5917064d7e8c1834acf679b0ccdd4242
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-19 16:17:36 -0600

    Add quizz card component

---
+++ src/lib/components/Quizz.svelte
@@ -0,0 +1,28 @@
+<script>
+
+
+    let {title, body, icon, alt, ...props} = $props();
+</script>
+
+
+<div class="card bg-base-100 w-64 pt-4 shadow-xs shadow-white ">
+    <figure class="w-full m-auto">
+        <img
+            class="w-5/12 h-[100px]"
+            src={icon}
+            alt={alt}
+        />
+    </figure>
+
+    <div class="card-body">
+
+        <h2 class="card-title">{title}</h2>
+    
+        <p>{body}</p>
+    
+        <div class="card-actions justify-end">
+            <button class="btn btn-primary">Ver</button>
+        </div>
+
+    </div>
+</div>
