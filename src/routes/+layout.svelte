ef5213bbb8c9e55d8dfa013d77a2813e0bd0eb50
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-18 23:08:37 -0600

    add linux icon to navbar

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,18 @@
+<script>
+	import '../app.css';
+	import  linux_icon  from "$lib/linux-tux.svg";
+	
+	let { children } = $props();
+</script>
+
+<!-- navbar -->
+<nav>
+	
+	<div>
+		<img src={linux_icon} alt="linux icon">
+	</div>
+
+</nav>
+
+
+{@render children()}
