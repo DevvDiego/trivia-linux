d53c8e7544442854594b62a07d9a7f5addb8a6d8
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-24 17:45:19 -0600

    Fix: not storing the same key

--- src/lib/stores/currentQuizz.svelte.js
+++ src/lib/stores/currentQuizz.svelte.js
@@ -1,18 +1,18 @@
 //use this store to not depend or url or slug pages
 export let currentQuizz = $state({value:""});
 export const changeCurrentQuizz = (quizz)=>{
-    localStorage.setItem("currentQuizz", quizz);
+    localStorage.setItem("storedQuizz", quizz);
     currentQuizz.value = quizz;
 }
 
 export const getCurrentQuizz = () => {
     //que deberia hacer??
-    let previousQuizz = localStorage.getItem("storedQuizz");
+    const storedQuizz = localStorage.getItem("storedQuizz");
     
-    if( previousQuizz === null) throw new Error("no previous quizz");
+    if( storedQuizz === null) throw new Error("no previous quizz");
 
     //restore the previous quizz
-    currentQuizz.value = previousQuizz;
+    currentQuizz.value = storedQuizz;
     return currentQuizz.value;
 }
 
