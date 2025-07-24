d275e4da19d592830a6aff55d34ae88b96ccb92c
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-24 17:36:08 -0600

    Add func to store also the quizz in local storage

--- src/lib/stores/currentQuizz.svelte.js
+++ src/lib/stores/currentQuizz.svelte.js
@@ -1,6 +1,18 @@
 //use this store to not depend or url or slug pages
 export let currentQuizz = $state({value:""});
 export const changeCurrentQuizz = (quizz)=>{
+    localStorage.setItem("currentQuizz", quizz);
     currentQuizz.value = quizz;
+}
+
+export const getCurrentQuizz = () => {
+    //que deberia hacer??
+    let previousQuizz = localStorage.getItem("storedQuizz");
+    
+    if( previousQuizz === null) throw new Error("no previous quizz");
+
+    //restore the previous quizz
+    currentQuizz.value = previousQuizz;
+    return currentQuizz.value;
 }
 
