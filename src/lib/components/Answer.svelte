4b9d224031cd07fa67faea2b0972802b2cdd5e26
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-25 01:01:56 -0600

    Add Anwer component, similar to question but with a tooltip

---
+++ src/lib/components/Answer.svelte
@@ -0,0 +1,31 @@
+<script>
+    let { class: className, id, technology,
+         question, selected, score, reason, isCorrect} = $props();
+</script>
+
+<fieldset class="{className} 
+    {(isCorrect) ? "border-success":"border-error"}
+    fieldset bg-base-200 border-base-300 
+    rounded-box w-xs border p-4 my-5"
+>
+  
+    <legend class="fieldset-legend"> {technology} </legend>
+  
+    <h2 class="text-base font-base">
+        {question}
+    </h2>
+
+
+    <select class="select" onchange={storeValue}>
+        <option disabled selected>{selected}</option>
+    </select>
+
+
+    <p class="label"> {score} points </p>
+
+    <div class="m-auto tooltip tooltip-top text-wrap" data-tip={reason}>
+        <button class="btn">Explicacion</button>
+    </div>
+
+
+</fieldset>
