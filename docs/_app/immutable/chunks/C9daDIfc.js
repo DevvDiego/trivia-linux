fb0a32c5ae65687166fc98680de7bcd428d491c2
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-25 01:57:57 -0600

    add deploy for gh pages

---
+++ docs/_app/immutable/chunks/C9daDIfc.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_1m1gm70?.base??"/trivia-linux",a=globalThis.__sveltekit_1m1gm70?.assets??s;export{a,s as b};
---
+++ docs/_app/immutable/nodes/0.WLYHbbkl.js
@@ -0,0 +1,5 @@
+import"../chunks/DsnmJJEf.js";import{v,w as f,x as d,y as g,z as w,A as h,B as b,C as e,D as o,E as t,F as y}from"../chunks/B6VBc8lO.js";import{g as _}from"../chunks/IWHw-WP2.js";var z=f(`<nav class="sticky top-0 left-0 z-10"><ul class="
+		w-dvw h-14 py-3 px-5 rounded-b-xl
+		flex flex-row items-center justify-between md:justify-around
+		bg-base-300 text-white md:text-lg
+	"><li class="flex flex-row items-center gap-3 text-xl"><a href="/" class="btn"><div class="max-w-6 md:max-w-10"><img src="./icons/linuxtux.svg" alt="linux icon"/></div> Dev Quizzes</a></li> <li><span> </span></li></ul></nav> <!>`,1);function S(c,s){v(s,!0);const p=!0;var r=z(),a=d(r),l=e(a),i=o(e(l),2),n=e(i),x=e(n);t(n),t(i),t(l),t(a);var m=o(a,2);return g(m,()=>s.children),w(u=>y(x,`Score: ${u??""}`),[_]),h(c,r),b({prerender:p})}export{S as component};
