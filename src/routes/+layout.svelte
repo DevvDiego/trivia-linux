2d7f495a999291600f2e97e1f1457fcafaa047c0
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-25 09:40:00 -0600

    Fix content shifting

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -13,8 +13,8 @@
 <nav class="sticky top-0 left-0 z-10">
 	
 	<ul class="
-		w-dvw h-14 py-3 px-5 rounded-b-xl
-		flex flex-row items-center justify-between md:justify-around
+		w-dvw h-14 py-3 rounded-b-xl
+		flex flex-row items-center justify-around md:justify-around
 		bg-base-300 text-white md:text-lg
 	">
 		<li class="flex flex-row items-center gap-3 text-xl">
