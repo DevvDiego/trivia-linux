87ba00bbd371011c20dd33f9abdb291593ce9868
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-25 02:15:10 -0600

    updt

---
+++ docs/_app/immutable/chunks/BJZgI9Fg.js
@@ -0,0 +1,1 @@
+const s=globalThis.__sveltekit_sad8sa?.base??"/triviaLinux",a=globalThis.__sveltekit_sad8sa?.assets??s;export{a,s as b};
--- docs/_app/immutable/chunks/D3hMU70w.js
+++
@@ -1,1 +0,0 @@
-const s=globalThis.__sveltekit_1lksi7x?.base??"/trivia-linux",a=globalThis.__sveltekit_1lksi7x?.assets??s;export{a,s as b};
