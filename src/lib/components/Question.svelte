8c711f957655cc6845f9d7e90e6573fd65ad50fd
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-24 22:42:23 -0600

    Add basic question fieldset

---
+++ src/lib/components/Question.svelte
@@ -0,0 +1,45 @@
+<script>
+
+
+    let { class: className, id, technology, difficulty,
+         question, options, answer, score } = $props();
+</script>
+
+<fieldset class="{className} my-5
+    fieldset bg-base-200 border-base-300 rounded-box w-xs border p-4"
+>
+  
+    <legend class="fieldset-legend"> {technology} </legend>
+  
+    <h2 class="text-base font-base">
+        {question}
+    </h2>
+
+
+    <select class="select">
+        <option disabled selected>Selecciona una opcion</option>
+        {#each options as option}
+            <option>
+                {option}
+            </option>
+        {/each}
+    </select>
+
+
+    <p class="label"> {score} points </p>
+
+    {#if difficulty == "easy"}
+        <div class="badge badge-success">Easy</div>
+    {:else if difficulty == "medium"}
+        <div class="badge badge-warning">Medium</div>
+    {:else if difficulty == "hard"}
+        <div class="badge badge-error">Hard</div>
+    {:else}
+        <!-- Show no badges -->
+    {/if}
+
+    <button class="btn btn-primary mt-4">
+        Send
+    </button>
+
+</fieldset>
