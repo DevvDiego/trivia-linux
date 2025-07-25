c16ff715fffaa315f03e52bf1c6cc92b9d9296ab
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-24 21:52:47 -0600

    Add basic quizz page

---
+++ src/routes/quizz/+page.svelte
@@ -0,0 +1,28 @@
+<script>
+    import { getCurrentQuizz } from "$lib/stores/currentQuizz.svelte";
+    import { onMount } from "svelte";
+
+    let questions = $state(null);
+
+    onMount(async ()=>{
+        const response = await fetch( getCurrentQuizz() )
+        const quizz = await response.json();
+
+        console.log(quizz);
+    });
+
+
+</script>
+
+
+<div></div>
+
+
+<div class="absolute bottom-4 w-full text-center">
+    <ul class="steps">
+        <li class="step step"></li>
+        <li class="step step"></li>
+        <li class="step step"></li>
+        <li class="step step"></li>
+    </ul>
+</div>
