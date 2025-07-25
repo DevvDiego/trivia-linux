778f08cbc6c3bec6342d8e2285e063af6cfba0cf
devvdiego <83733071+DevvDiego@users.noreply.github.com>
2025-07-24 22:42:00 -0600

    Add iterable questions

--- src/routes/quizz/+page.svelte
+++ src/routes/quizz/+page.svelte
@@ -1,4 +1,5 @@
 <script>
+    import Question from "$lib/components/Question.svelte";
     import { getCurrentQuizz } from "$lib/stores/currentQuizz.svelte";
     import { onMount } from "svelte";
 
@@ -8,17 +9,22 @@
         const response = await fetch( getCurrentQuizz() )
         const quizz = await response.json();
 
-        console.log(quizz);
+        questions = quizz;
+
     });
 
 
 </script>
 
 
-<div></div>
+{#each questions as question}
+    
+    <Question class="m-auto" {...question} />
+
+{/each}
 
 
-<div class="absolute bottom-4 w-full text-center">
+<div class="bg-base-100 fixed bottom-0 w-full text-center">
     <ul class="steps">
         <li class="step step"></li>
         <li class="step step"></li>
